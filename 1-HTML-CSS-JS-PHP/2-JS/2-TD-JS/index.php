1a923df024d8996a3caf64eac3f66092679b74f2
yugmerabtene <yug.merabtene@gmail.com>
2023-12-21 23:58:29 +0100

    Ynov0.1
    Ynov0.1

---
+++ 1-HTML-CSS-JS-PHP/2-JS/2-TD-JS/index.php
@@ -0,0 +1,33 @@
+<!-- INCLUDE DU HEADER-->
+<?php include 'header.html';?>
+    <main>
+        <!-- DEBUT SLIDER -->
+        <div id="slider">
+            <img src="" alt="">
+        </div>
+        <!-- FIN SLIDER -->
+        <!-- DEBUT SECTION -->
+        <section>
+            <article>
+                <img src="" alt="">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At autem,
+                    dolorem eveniet fugit ipsam iure nesciunt numquam pariatur quidem
+                    similique totam ut vel. Aspernatur beatae fuga necessitatibus placeat
+                    repellendus, soluta.
+                </p>
+            </article>
+            <article>
+                <img src="" alt="">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At autem,
+                    dolorem eveniet fugit ipsam iure nesciunt numquam pariatur quidem
+                    similique totam ut vel. Aspernatur beatae fuga necessitatibus placeat
+                    repellendus, soluta.
+                </p>
+            </article>
+        </section>
+        <!-- FIN SECTION -->
+    </main>
+<!-- INCLUDE DU FOOTER-->
+<?php include 'footer.html';?>
+
+
