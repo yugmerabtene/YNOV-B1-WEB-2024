bf548e50c0634c50e8f01450e29c1b6224f4f4de
yugmerabtene <yug.merabtene@gmail.com>
2023-12-23 14:08:51 +0100

    Mise à jour

--- 1-HTML-CSS-JS-PHP/2-JS/2-TD-JS/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const hamburger = document.querySelector('.hamburger');
-    const nav = document.querySelector('nav');
-
-    hamburger.addEventListener('click', function () {
-        hamburger.classList.toggle('active');
-        nav.classList.toggle('show');
-    });
-});
